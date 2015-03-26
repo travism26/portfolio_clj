342d94f3b0c7f15fb0e42d3835e52027dff01863
travism26 <travismartin266@gmail.com>
2015-03-26 16:47:54 -0300

    updated portfolio

--- resources/public/layout.clj
+++ resources/public/layout.clj
@@ -81,6 +81,23 @@
          [:p "SOME DESCRIPTION"]
          ]]]]]]])
 
+(defn services-layout []
+  [:section {:id "service" :class "services bg-primary"}
+   [:div {:class "container"}
+    [:div {:class "row text-center"}
+     [:div {:class "col-lg-10 col-lg-offset-1"}
+      [:h2 "My Services"]
+      [:hr {:class "small"}]
+      [:div {:class "row"}
+       [:div {:class "col-md-3 col-sm-6"}
+        [:div {:class "service-item"}
+         [:span {:class "fa-stack fa-4x"}
+          [:i {:class "fa fa-circle fa-stack-2x"}]
+          [:i {:class "fa fa-cloud fa-stack-1x text-primary"}]]
+         [:h4 [:stong "SOME TITLE"]]
+         [:p "SOME DESCRIPTION"]
+         ]]]]]]])
+
 ;<div class="col-lg-10 col-lg-offset-1">
 
 ; keeping just in case i break something :)
